b65a6537b4e82543fe2b833b7c691b334088b7ee
Zamay <mityazamay@gmail.com>
2017-03-20 00:02:34 +0200

    финал версия v1.3

--- js/time_user.js
+++ js/time_user.js
@@ -3,7 +3,6 @@
  */
 
 window.onload = function () {
-
     //пользователей онлайн
     (function () {
         var usersOnline = $(".list_user>ul>li").length;
@@ -25,20 +24,17 @@ window.onload = function () {
 };
 
 //колво символов
-
 function text_button() {
     $('#editor').keyup(function () {
 
         var count = $(this).text().length;
         $('.info>ul>li')[0].innerHTML = (count + ' characters entered');
 
-
         var words = $(this).text().split(/[\S\.\?]+/).length - 1;
         $('.info>ul>li')[1].innerHTML = (words + ' letters entered');
 
         var whitespaces = $(this).text().split(/\s/).length - 1;
         $('.info>ul>li')[2].innerHTML = (whitespaces + ' whitespace characters entered');
-
 
         var marks = $(this).text().split(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/).length - 1;
         $('.info>ul>li')[3].innerHTML = (marks + ' punttuation marks entered');
@@ -51,6 +47,9 @@ var date = new Date();
 function user_time() {
     $('.online_for').html("You are online for : " + moment(date).toNow(true));
     $('.local_time p').html("Your local time is : " + moment().format('H:mm:ss'));
+
+    $('.onlaine_user').html("Online: " + moment(date).toNow(true));
+    $('.time').html("Time is: " + moment().format('H:mm:ss'));
 }
 setInterval(user_time, 1000);
 
